9659a6b195ab0ec802f5c336eece6fbe5815c764
dioscar Rodriguez <dioscarr@gmail.com>
2020-01-04 10:39:52 -0500

    Added ejs

--- app.js
+++ app.js
@@ -5,23 +5,16 @@ const bodyParser = require('body-parser');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
-const expressHbs = require('express-handlebars');
-
-
-
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')));
 
-// app.engine('hbs',expressHbs());
- app.set('view engine', 'pug');
- app.set('views','views');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(rootDir,'public')));
-
-app.use('/admin',adminData.routes);
-app.use('/',shopRoutes);
-app.use((req,res,next)=>{
-    res.status(404).render('404',{message:'Page Not Found1'});
+app.use('/admin', adminData.routes);
+app.use('/', shopRoutes);
+app.use((req, res, next) => {
+    res.status(404).render('404', { PageTitle: '404er', message: 'Page Not Found1' });
 });
 
 
